8386552107d3357de249644848d2f36bb3f8a0bf
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2022-05-19 19:54:45 +0800

    修复图片漂移问题

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,13 +1,37 @@
+// 图片
+
+var images = document.querySelectorAll(".md-typeset img"); // , .md-typeset svg
+
+for (var i = 0; i < images.length; i++)
+  (
+    images[i].clientWidth  > 80 ||
+    images[i].clientHeight > 80
+  ) &&
+  images[i].classList.add("image-block");
+
+// in extra.css:
+// .image-block { display: block; margin: .5em auto; }
+
+
 // 页面底部 更新/创建日期：改善用词，本地化符号
-var sourceFile = document.querySelector('article > .md-source-file:last-child > small');
-if (sourceFile !== null && sourceFile.childNodes.length > 0) {
-  var nodes = sourceFile.childNodes;
+
+var container = document.querySelector("article > .md-source-file:last-child > small");
+
+if (
+  container !== null &&
+  container.childNodes.length >  0 &&
+  container.childNodes.length <= 16
+) {
+
+  var nodes = container.childNodes;
   var replaces = [ [ "最后更新", "更新于" ], [ "创建日期", "创建于" ] ];
-  for (var i = 0; i < nodes.length; i++)
+
+  for (var i = 0; i < nodes.length; i++) 
     if (nodes[i].nodeType === 3) // Node.TEXT_NODE === 3
       for (var j = 0; j < replaces.length; j++)
         if (nodes[i].data.indexOf(replaces[j][0]))
           nodes[i].data = nodes[i].data
             .replace(replaces[j][0], replaces[j][1])
             .replace(/:\s+/, "：");
+
 }
